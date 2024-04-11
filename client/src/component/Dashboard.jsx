4244e7c829bdf2159e43bba0614091a009e3cd3c
manace07 <joannajodelacruz@gmail.com>
2024-04-11 10:54:56 +0800

    first commit

---
+++ client/src/component/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className='bg-white text-slate-500 p-10 text-center'>
+      <h1>Dashboard</h1>
+      <p>This is dashboard</p>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ client/src/component/share/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar w-60 bg-blue-500 text-white p-24 float-left'>
+      <h2>Sidebar</h2>
+      <p>this is a sidebar</p>
+    </div>
+  )
+}
+
+export default Sidebar
